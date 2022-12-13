567ecd6e852c2f0e76c701c5b9b9f1d20128a2bd
kTz1 <catalin_simon90@yahoo.com>
2022-12-13 15:39:49 +0200

    added layout

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,7 +1,46 @@
-import React from "react"
+
+import React from 'react';
+import { useEthers } from '@usedapp/core';
+
+import styles from './styles';
+import { uniswapLogo } from './assets';
 
 const App = () => {
-  return <p>Hello, Web3!</p>
-}
+  const { account } = useEthers();
+
+  const poolsLoading = false;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <header className={styles.header}>
+          <img src={uniswapLogo} alt="uniswap logo" className="w-16 h-16 object-contain" />
+          <WalletButton />
+        </header>
+        
+        <div className={styles.exchangeContainer}>
+          <h1 className={styles.headTitle}>Uniswap 2.0</h1>
+          <p className={styles.subTitle}>Exchange tokens in seconds</p>
+
+          <div className={styles.exchangeBoxWrapper}>
+            <div className={styles.exchangeBox}>
+              <div className="pink_gradient" />
+              <div className={styles.exchange}>
+                <div>
+                  {account ? (
+                    poolsLoading ? (
+                      <Loader />
+                    ) : <Exchange />
+                  ) : <Loader />}
+                </div>
+              </div>
+              <div className="bule_gradient" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
